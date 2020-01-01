afb371fe6752c861c323966906d48c767f019e8f
Steven Zhu <stevenzhu03@gmail.com>
2020-01-01 04:09:40 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -23,4 +23,11 @@ while (i < source_array.length) do
   i += 1
 end
 
+map_to_square(source_array)
+i = 0 
+result = []
 
+while (i < source_array.length) do
+  result << source_array[i] * 2
+  i += 1
+end
