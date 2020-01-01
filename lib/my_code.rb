139a7dbd3193d449de15eb1e60249c7d8589ce48
Steven Zhu <stevenzhu03@gmail.com>
2020-01-01 04:29:50 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -70,8 +70,8 @@ end
 def reduce_to_any_true(source_array)
   i = 0
   while i < source_array.length do
-    return true if !source_array[i]
+    return true if source_array[i]
     i += 1
   end
-  return true
+  return false
 end
