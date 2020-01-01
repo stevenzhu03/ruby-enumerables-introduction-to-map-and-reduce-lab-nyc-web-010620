de70b484e3d605b1f620acd7ec16bdb798a06fb3
Steven Zhu <stevenzhu03@gmail.com>
2020-01-01 04:21:27 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -48,3 +48,12 @@ new = starting_point
   return new
 end
 
+def reduce_to_total(source_array, starting_point = 0)
+new = starting_point
+  i = 0
+  while i < source_array.length do
+    new += source_array[i]
+    i += 1
+  end
+  return new
+end
