ad60d1f5cad794984004c9ae78cf6e4b7587b143
Steven Zhu <stevenzhu03@gmail.com>
2020-01-01 04:09:14 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -14,3 +14,13 @@ def map_to_no_change(source_array)
   source_array
 end
 
+map_to_double(source_array)
+i = 0 
+result = []
+
+while (i < source_array.length) do
+  result << source_array[i] * 2
+  i += 1
+end
+
+
